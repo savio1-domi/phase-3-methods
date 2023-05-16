a5ffb6d5429662b2122cc6a624acf8d9ee9aa29a
git-savio1.domi <techsavvydominic3@gmail.com>
2023-05-16 10:25:15 +0300

    Completed assignments

--- methods.rb
+++ methods.rb
@@ -1,1 +1,34 @@
 # Your code here!
+# Problem 1
+def greet_programmer
+    puts "Hello, programmer!"
+end
+greet_programmer
+
+# Problem 2
+def greet(name)
+    puts "Hello, #{name}!"
+end
+greet("Naureen")
+
+# Problem 3
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default("Naureen")
+
+# Problem 4
+def add(num1, num2)
+    return num1 + num2
+end
+add(1,2)
+
+# Problem 5
+def halve(x)
+    if x.is_a?(Integer)
+        x / 2 
+    else
+        nil
+    end
+end
+halve(6) 
